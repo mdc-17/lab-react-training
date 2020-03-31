5c3c11e875a7cd28eeb4dee889d19b8602955c2c
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-31 16:53:31 +0200

    fixedBoxColor

--- src/boxcolor/BoxColor.js
+++ src/boxcolor/BoxColor.js
@@ -5,7 +5,19 @@ const BoxColor = ({r, g, b}) => {
     const divStyle = {
         background: `rgb(${r},${g},${b})`
     };
-    return <div className="boxColor-container" style={divStyle}></div>
+    
+    function componentToHex(c) {
+        let hex = c.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    }
+
+    function rgbText (r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+    return <div className="boxColor-container" style={divStyle}>
+        <h1 className="num1">RGB({r},{g},{b})</h1>
+        <p className="num2">{rgbText(r,g,b)}</p>
+    </div>
 };
 
 
